a2283fae3eea4c7675d04d1f5e174acecfe73290
Jordan <hello@jordan-massart.be>
2020-04-16 16:47:05 +0200

    doc(meilisearch): add PHPDoc on every function

--- src/Engines/MeilisearchEngine.php
+++ src/Engines/MeilisearchEngine.php
@@ -16,6 +16,12 @@ class MeilisearchEngine extends Engine
         $this->softDelete = $softDelete;
     }
 
+    /**
+     * Update the given model in the index.
+     *
+     * @param  \Illuminate\Database\Eloquent\Collection  $models
+     * @return void
+     */
     public function update($models)
     {
         if ($models->isEmpty()) {
@@ -41,6 +47,12 @@ class MeilisearchEngine extends Engine
         }
     }
 
+    /**
+     * Remove the given model from the index.
+     *
+     * @param  \Illuminate\Database\Eloquent\Collection  $models
+     * @return void
+     */
     public function delete($models)
     {
         $index = $this->meilisearch->getIndex($models->first()->searchableAs());
@@ -52,6 +64,12 @@ class MeilisearchEngine extends Engine
         );
     }
 
+    /**
+     * Perform the given search on the engine.
+     *
+     * @param  \Laravel\Scout\Builder  $builder
+     * @return mixed
+     */
     public function search(\Laravel\Scout\Builder $builder)
     {
         return $this->performSearch($builder, array_filter([
@@ -59,6 +77,14 @@ class MeilisearchEngine extends Engine
         ]));
     }
 
+    /**
+     * Perform the given search on the engine.
+     *
+     * @param  \Laravel\Scout\Builder  $builder
+     * @param  int  $perPage
+     * @param  int  $page
+     * @return mixed
+     */
     public function paginate(\Laravel\Scout\Builder $builder, $perPage, $page)
     {
         return $this->performSearch($builder, array_filter([
@@ -66,6 +92,13 @@ class MeilisearchEngine extends Engine
         ]));
     }
 
+    /**
+     * Perform the given search on the engine.
+     *
+     * @param  \Laravel\Scout\Builder  $builder
+     * @param  array  $options
+     * @return mixed
+     */
     protected function performSearch(\Laravel\Scout\Builder $builder, array $options = [])
     {
         $meilisearch = $this->meilisearch->getIndex($builder->index ?: $builder->model->searchableAs());
@@ -82,6 +115,12 @@ class MeilisearchEngine extends Engine
         return $meilisearch->search($builder->query, $options);
     }
 
+    /**
+     * Pluck and return the primary keys of the given results.
+     *
+     * @param  mixed  $results
+     * @return \Illuminate\Support\Collection
+     */
     public function mapIds($results)
     {
         $hits = collect($results['hits']);
@@ -90,6 +129,14 @@ class MeilisearchEngine extends Engine
         return $hits->pluck($key)->values();
     }
 
+    /**
+     * Map the given results to instances of the given model.
+     *
+     * @param  \Laravel\Scout\Builder  $builder
+     * @param  mixed  $results
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public function map(\Laravel\Scout\Builder $builder, $results, $model)
     {
         if (is_null($results) || count($results['hits']) === 0) {
@@ -108,11 +155,23 @@ class MeilisearchEngine extends Engine
         })->values();
     }
 
+    /**
+     * Get the total count from a raw result returned by the engine.
+     *
+     * @param  mixed  $results
+     * @return int
+     */
     public function getTotalCount($results)
     {
         return $results['nbHits'];
     }
 
+    /**
+     * Flush all of the model's records from the engine.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
     public function flush($model)
     {
         $index = $this->meilisearch->getIndex($model->searchableAs());
@@ -120,11 +179,24 @@ class MeilisearchEngine extends Engine
         $index->deleteAllDocuments();
     }
 
+    /**
+     * Determine if the given model uses soft deletes.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return bool
+     */
     protected function usesSoftDelete($model)
     {
         return in_array(Illuminate\Database\Eloquent\SoftDeletes::class, class_uses_recursive($model));
     }
 
+    /**
+     * Dynamically call the MeiliSearch client instance.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     */
     public function __call($method, $parameters)
     {
         return $this->meilisearch->$method(...$parameters);
