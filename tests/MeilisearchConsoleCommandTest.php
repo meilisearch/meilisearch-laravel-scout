0703fb46a16a7cef25b789e1915de046ec4c6c8c
Markus Machatschek <mmachatschek@yahoo.com>
2021-02-01 11:29:29 +0100

    Add index console command test

---
+++ tests/MeilisearchConsoleCommandTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Meilisearch\Scout\Tests;
+
+use Laravel\Scout\EngineManager;
+use MeiliSearch\Client;
+use MeiliSearch\Endpoints\Indexes;
+use MeiliSearch\Exceptions\HTTPRequestException;
+use Meilisearch\Scout\Engines\MeilisearchEngine;
+use Mockery as m;
+
+class MeilisearchConsoleCommandTest extends TestCase
+{
+    /** @test */
+    public function nameArgumentIsRequired()
+    {
+        $this->expectExceptionMessage('Not enough arguments (missing: "name").');
+        $this->artisan('scout:index')
+            ->execute();
+    }
+
+    /** @test */
+    public function commandCreatesIndex()
+    {
+        $client = $this->mock(Client::class);
+        $client->expects('createIndex')->with($indexName = 'testindex', [])->andReturn(m::mock(Indexes::class));
+
+        $engineManager = $this->mock(EngineManager::class);
+        $engineManager->shouldReceive('engine')->with('meilisearch')->andReturn(new MeilisearchEngine(
+            $client
+        ));
+
+        $this->artisan('scout:index', [
+            'name' => $indexName,
+        ])
+            ->expectsOutput('Index "'.$indexName.'" created.')
+            ->assertExitCode(0)
+            ->run();
+    }
+
+    /** @test */
+    public function keyParameterSetsPrimaryKeyOption()
+    {
+        $client = $this->mock(Client::class);
+        $client
+            ->expects('createIndex')
+            ->with($indexName = 'testindex', ['primaryKey' => $testPrimaryKey = 'foobar'])
+            ->andReturn(m::mock(Indexes::class));
+
+        $engineManager = $this->mock(EngineManager::class);
+        $engineManager->shouldReceive('engine')->with('meilisearch')->andReturn(new MeilisearchEngine(
+            $client
+        ));
+
+        $this->artisan('scout:index', [
+            'name' => $indexName,
+            '--key' => $testPrimaryKey,
+        ])
+            ->expectsOutput('Index "'.$indexName.'" created.')
+            ->assertExitCode(0)
+            ->run();
+    }
+
+    /** @test */
+    public function deleteParameterDeletesIndex()
+    {
+        $client = $this->mock(Client::class);
+        $client->expects('deleteIndex')->with($indexName = 'testindex')->andReturn([]);
+
+        $engineManager = $this->mock(EngineManager::class);
+        $engineManager->shouldReceive('engine')->with('meilisearch')->andReturn(new MeilisearchEngine(
+            $client
+        ));
+
+        $this->artisan('scout:index', [
+            'name' => $indexName,
+            '--delete' => true,
+        ])
+            ->expectsOutput('Index "'.$indexName.'" deleted.')
+            ->assertExitCode(0)
+            ->run();
+    }
+
+    /** @test */
+    public function commandReturnsErrorStatusCodeOnException()
+    {
+        $client = $this->mock(Client::class);
+        $client->expects('createIndex')->andThrow(new HTTPRequestException(404, ['message' => 'Testmessage']));
+
+        $engineManager = $this->mock(EngineManager::class);
+        $engineManager->shouldReceive('engine')->with('meilisearch')->andReturn(new MeilisearchEngine($client));
+
+        $this->artisan('scout:index', [
+            'name' => 'foobar',
+        ])
+            ->assertExitCode(0)
+            ->execute();
+    }
+}
